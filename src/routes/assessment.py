774ace6e463ad630927158c7c3488592fc3e89aa
Andrew-exe <andrew.zhou.su@gmail.com>
2025-09-08 21:59:39 -0700

    assessment feature (in progress)

--- src/routes/assessment.py
+++ src/routes/assessment.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from fastapi import status
 
-from backend.src.models.requests import AssessmentRequest
-from backend.src.models.responses import AssessmentResponse
+import time
+
+from backend.src.models.requests import AssessmentRequest, UserGraphRequest
+from backend.src.models.responses import GiveAssessmentResponse, AssessmentResultResponse, AssessmentStoreResponse, UserGraphResponse, RouteResponse
 from backend.src.services.assessment_service import AssessmentService
 
 router = APIRouter()
@@ -13,23 +15,39 @@ async def health_check():
     """Health check endpoint"""
     return {"status": "ok"}
 
-@router.get("/assessment/{student_id}", response_model = AssessmentResponse)
-async def give_assessment(student_id: int):
+@router.post("/check/{student_id}", response_model=RouteResponse)
+async def route_student(student_id: int):
+    """Check if need to give assessment to new lesson or if assessment already done"""
+    route = await assessment_service.check_need_assessment(student_id)
+    if route: 
+        return RouteResponse(give_assessment=True)
+    else:
+        return RouteResponse(give_assessment=False)
+
+@router.get("/give_assessment", response_model = GiveAssessmentResponse)
+async def give_assessment():
     """give assessment test to new student"""
-    # problems_dict = await assessment_service.get_assessment()
+    problems_list = await assessment_service.get_assessment()
 
-    # return AssessmentResponse(problems=problems_dict, student_id=student_id)
-    return AssessmentResponse(problems="assessment test", student_id=student_id)
+    return GiveAssessmentResponse(problems=problems_list, number_problems=len(problems_list), start_time=time.time())
 
+@router.post("/submit", response_model = AssessmentResultResponse)
+async def assessment_check(request: AssessmentRequest):
+    """check assessment answers and return solutions"""
+    result, number_correct = await assessment_service.evaluate_assessment(request.student_answers)
+    
+    return AssessmentResultResponse(solutions=result, total_correct=number_correct, duration_seconds=time.time() - request.start_time)
 
-@router.post("/assessment_check/{student_id}/", status_code=status.HTTP_204_NO_CONTENT)
-async def assessment_check(student_id: int, request: AssessmentRequest):
-    """check assessment answers and update student knowledge graph"""
-    # list_of_score = await assessment_service.evaluate_assessment(request.student_answers)
-    return {student_id: "assessment checked"}
+@router.post("/store_results/{student_id}/", response_model = AssessmentStoreResponse)
+async def assessment_store(student_id: int, request: AssessmentRequest):
+    id = await assessment_service.store_student_score(student_id, request.student_answers)
 
-@router.post("/assessment_store/{student_id}/", status_code=status.HTTP_204_NO_CONTENT)
-async def assessment_store(student_id: int):
-    # await assessment_service.store_student_score(student_id, list_of_score)
-    return {student_id: "assessment answer stored"}
+    return AssessmentStoreResponse(assessment_id=str(id))
+
+@router.post("/update_knowledge/{assessment_id}/", response_model=UserGraphResponse)
+async def update_knowledge_graph(assessment_id: str, request: UserGraphRequest):
+    """Update student knowledge graph based on assessment results"""
+    updated_graph = await assessment_service.update_graph(assessment_id, request.user_graph)
+
+    return UserGraphResponse(user_graph=updated_graph)
     
--- src/services/assessment_service.py
+++ src/services/assessment_service.py
@@ -1,65 +1,140 @@
 from backend.src.utils import logging
 from backend.src.services.MongoDBHandler import MongoDBHandler
 import random
+from bson import ObjectId
+from backend.src.services.bkt import BayesianKnowledgeTracing
+from backend.src.utils import knowledge_info
+
 
 class AssessmentService:
     def __init__(self):
         self.logger = logging.set_logger(__name__)
         self.db = MongoDBHandler("mongodb://172.16.0.177:27019")
         self.db.connect('amc8_database')
-        self.problems = {}
+        self.bkt = BayesianKnowledgeTracing(knowledge_info.amc8_concepts)
+
+    async def check_need_assessment(self, student_id: int):
+        """Check if student needs to take assessment for new lesson"""
+        # Check if student has taken assessment before
+        assessments = self.db.find_documents('assessments', {"student_id": student_id})
+        if not assessments:
+            logging.log(f"Student {student_id} has not taken any assessments. Needs to take assessment.", self.logger, 1)
+            return True
+        else:
+            logging.log(f"Student {student_id} has already taken assessments. No need to take assessment.", self.logger, 1)
+            return False
 
     async def get_assessment(self):
         """Retrieve assessment problems from the database"""
+        problems_list = []
 
         # Select 25 random problems from the database
+        logging.log("Fetching assessment problems from database", self.logger, 1)
+
         for i in range(1,26):
-            problems = self.db.find_documents('problems', {"problem_numer": i}, ['difficulty', 'concepts', 'problem', 'answer'])
-            index = random.randint(0,len(problems)-1)
-            problem = problems[index]
-            self.problems[problem.get("_id")] = problem
+            problems = self.db.find_documents('problems', {"problem_number": i}, ['problem_number', 'problem'])
+            problem = random.choice(problems)
+            problem['_id'] = str(problem['_id'])
+            problems_list.append(problem)
 
-        # return only the problem text for displaying the assessment
-        assessment_problems = {}
-        for key,value in self.problems.items():
-            assessment_problems[key] = value.get("problem")
 
-        return assessment_problems
+        if len(problems_list) != 25:
+            logging.log(f"Error: Retrieved {len(problems_list)} problems instead of 25", self.logger, 0)
+        else:   
+            logging.log("Successfully retrieved 25 assessment problems", self.logger, 1)
+        
+        return problems_list
 
     async def evaluate_assessment(self, student_answers: dict):
         """Evaluate student's answers and return score for bkt algorithm to update student knowledge graph"""
         student_score = []
-        for problem_id, student_answer in student_answers.items():
-            if self.problems[problem_id].get("answer") == student_answer:
-                student_score.append({"concepts": self.problems[problem_id].get("concepts"), "difficulty": self.problems[problem_id].get("difficulty"), "correct": True})
+        for student_answer in student_answers:
+            id = ObjectId(student_answer.get("problem_id"))
+            answer = student_answer.get("answer")
+            problem = self.db.find_documents('problems', {"_id": id}, ['correct_answer', 'solution'])
+            if answer.lower() == problem.get('correct_answer').lower():
+                student_score.append({"_id": str(id), "correct": True, "correct_answer": problem.get('correct_answer'), "solution": problem.get('solution')})
             else:
-                student_score.append({"concepts": self.problems[problem_id].get("concepts"), "difficulty": self.problems[problem_id].get("difficulty"), "correct": False})
+                student_score.append({"_id": str(id), "correct": False, "correct_answer": problem.get('correct_answer'), "solution": problem.get('solution')})
 
-        return student_score
+        number_correct = 0
+        for i in student_score:
+            if i.get("correct"):
+                number_correct += 1
+
+        return student_score, number_correct
     
     async def store_student_score(self, student_id: int, assessment_score: list):
         """Store the assessment results in the database"""
+        assessment_id = str(student_id) + "_" + str(ObjectId())
         try:
-            assessment_record = {
-            "student_id": student_id,
-            "answers": assessment_score
-            }
-
-            self.db.insert_document('assessments', assessment_record)
+            for score in assessment_score:
+                self.db.insert_document('assessments', {
+                    "student_id": student_id,
+                    "assessment_id": assessment_id,
+                    "problem_id": score.get("_id"),
+                    "student_answer": score.get("answer"),
+                })
             logging.log(f"Stored assessment results for student {student_id}", self.logger, 1)
+
         except Exception as e:
             logging.log(f"Failed to store assessment results for student {student_id}: {e}", self.logger, 0)
 
-    async def retrieve_student_score(self, student_id: int):
-        """Retrieve the assessment results from the database"""
-        try:
-            assessment = self.db.find_documents('assessments', {"student_id": student_id})
-            if assessment:
-                logging.log(f"Retrieved assessment results for student {student_id}", self.logger, 1)
-                return assessment[-1]
-            else:
-                logging.log(f"No assessment results found for student {student_id}", self.logger, 0)
+        return assessment_id
+
+    async def update_graph(self, assessment_id: str, user_graph: dict):
+        try: 
+            if not user_graph:
+                user_graph = knowledge_info.amc8_knowledge_graph
+
+            assessment = self.db.find_documents('assessments', {"assessment_id": assessment_id})
+
+            if not assessment:
+                logging.log(f"No assessment found with id {assessment_id}", self.logger, 0)
                 return None
+            
+            student_id = assessment[0].get("student_id")
+
+            # Logic to update the student's knowledge graph based on assessment results
+            for problem in assessment:
+                grade = {}
+                problem_id = ObjectId(problem.get("problem_id"))
+                student_answer = problem.get("student_answer")
+                problem_data = self.db.find_documents('problems', {"_id": problem_id}, ['concepts', 'difficulty', 'correct_answer'])
+                concepts = problem_data.get('concepts')
+                difficulty = problem_data.get('difficulty')
+                correct_answer = problem_data.get('correct_answer')
+
+                for concept in concepts:
+                    grade[concept] = "correct" if student_answer == correct_answer else "incorrect"
+                # Update student knowledge graph with assessment results
+                # Using different damping factors based on difficulty
+                correct_damping = 0.2 if difficulty == 1 else 0.4 if difficulty == 2 else 0.5 if difficulty == 3 else 0.7 if difficulty == 4 else 0.85
+                incorrect_damping = 0.7 if difficulty == 1 else 0.5 if difficulty == 2 else 0.4 if difficulty == 3 else 0.3 if difficulty == 4 else 0.15
+                if student_answer == correct_answer:
+                    user_graph = self.bkt.bkt_algorithm(grade, user_graph, correct_damping)
+                else:
+                    user_graph = self.bkt.bkt_algorithm(grade, user_graph, incorrect_damping)
+
+            # self.db.insert_document('user_graphs', {"student_id": student_id, "assessment_id": assessment_id, "user_graph": user_graph})
+            logging.log(f"Updated knowledge graph for assessment {assessment_id}", self.logger, 1)
+
+            return user_graph
+
         except Exception as e:
-            logging.log(f"Failed to retrieve assessment results for student {student_id}: {e}", self.logger, 0)
+            logging.log(f"Failed to update knowledge graph for assessment {assessment_id}: {e}", self.logger, 0)
             return None
+            
+    # async def retrieve_student_score(self, student_id: int):
+    #     """Retrieve the assessment results from the database"""
+    #     try:
+    #         assessment = self.db.find_documents('assessments', {"student_id": student_id})
+    #         if assessment:
+    #             logging.log(f"Retrieved assessment results for student {student_id}", self.logger, 1)
+    #             return assessment[-1]
+    #         else:
+    #             logging.log(f"No assessment results found for student {student_id}", self.logger, 0)
+    #             return None
+    #     except Exception as e:
+    #         logging.log(f"Failed to retrieve assessment results for student {student_id}: {e}", self.logger, 0)
+    #         return None

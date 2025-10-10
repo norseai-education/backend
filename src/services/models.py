9f02740b473fa5320b5f3550e2321d28453a7d1a
ycgq <ycgq@yahoo.com>
2025-10-10 00:19:55 -0700

    Update models.py

--- src/services/models.py
+++ src/services/models.py
@@ -5,14 +5,13 @@ from langchain_ollama import OllamaEmbeddings
 LOCAL = False
 
 if not LOCAL:
-    classifier_model = OllamaLLM(model="qwen3:30b-a3b-instruct-2507-q4_K_M"，base_url="http://host.docker.internal:11434")
-    teacher_model = OllamaLLM(model="qwen3:30b-a3b-instruct-2507-q4_K_M"，base_url="http://host.docker.internal:11434")
-    evaluator_model = OllamaLLM(model="qwen3:30b-a3b-instruct-2507-q4_K_M"，base_url="http://host.docker.internal:11434")
+    classifier_model = OllamaLLM(model="qwen3:30b-a3b-instruct-2507-q4_K_M")
+    teacher_model = OllamaLLM(model="qwen3:30b-a3b-instruct-2507-q4_K_M")
+    evaluator_model = OllamaLLM(model="qwen3:30b-a3b-instruct-2507-q4_K_M")
     
 else:
     classifier_model = OllamaLLM(model="qwen3:4b")
     teacher_model = OllamaLLM(model="qwen3:4b")
     evaluator_model = OllamaLLM(model="qwen3:4b")
 
-embedding_model = OllamaEmbeddings(model="nomic-embed-text:latest"，base_url="http://host.docker.internal:11434")
-
+embedding_model = OllamaEmbeddings(model="nomic-embed-text:latest")

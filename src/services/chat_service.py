6d074c8cda4e6f04453b8dbf6ac4ce989307d2d7
Andrew-exe <andrew.zhou.su@gmail.com>
2025-11-07 11:12:05 -0800

    add logging

--- src/services/chat_service.py
+++ src/services/chat_service.py
@@ -192,6 +192,8 @@ class ChatService:
             # grab final graph state
             logging.log("Grabbing final graph state...", self.logger, 1)
             user_state = state_manager.get_redis_state()
+            logging.log(f"Retrieved state from redis: \n{user_state}", self.logger, 1)
+            
             user_state["messages"] = utils.convert_redis_messages(user_state["messages"])
             
             # Update session state
